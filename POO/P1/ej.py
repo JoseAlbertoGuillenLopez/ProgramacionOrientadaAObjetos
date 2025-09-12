1a0e7097735a0966ee1aab2f8e128fc0ac4e47b0
JoseGuillen <jose_3141240120@utd.edu.mx>
2025-09-12 15:45:29 -0600

    Delete POO/P1 directory

--- POO/P1/ej.py
+++
@@ -1,8 +0,0 @@
-def sum(base,altura):
-    suma=base*altura
-    return suma
-
-base=float(input("Tamaño de la base: "))
-altura=float(input("Tamaño de la altura: "))
-area=sum(base,altura)
-print(f"El area del rectangulo es: {area}")
